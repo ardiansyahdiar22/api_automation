5d33af195ff013e12b5ee935a7dc192e0f83b511
diarardiansyah <ardiansyahdiar@gmail.com>
2022-01-28 21:52:24 +0700

    add new folder automation api

---
+++ pytest_api/api_airport/test_get_favorit.py
@@ -0,0 +1,36 @@
+import json
+from pydoc import resolve
+import re
+from urllib import response
+import requests
+import pytest
+
+def test_get_fav_all():
+    auth = {'Authorization' : 'Bearer token=TV2PAY7e4UCvT64zxRLhK64t'}
+    url = ('https://airportgap.dev-tester.com/api/favorites')
+
+    response = requests.get(url, headers=auth)
+    print(response.text)
+    assert response.status_code == 200
+
+def test_get_fav_id():
+    auth = {'Authorization' : 'Bearer token=TV2PAY7e4UCvT64zxRLhK64t'}
+    airport_id = "4063"
+    url = (f"https://airportgap.dev-tester.com/api/favorites/{airport_id}")
+
+    response = requests.get(url, headers=auth)
+    assert response.status_code == 200
+    data = response.json().get("data") # <== Parsing data from JSON
+    assert data["id"] == "4063" 
+    assert data["attributes"]["airport"]["country"] == "Greenland"
+
+def test_get_fav_not_found():
+    auth = {'Authorization' : 'Bearer token=TV2PAY7e4UCvT64zxRLhK64t'}
+    airport_id = "4052"
+    url = (f"https://airportgap.dev-tester.com/api/favorites/{airport_id}")
+
+    response = requests.get(url, headers=auth)
+    assert response.status_code == 404
+    assert response.text
+
+     
